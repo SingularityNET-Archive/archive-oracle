c95edda41fd212859cd0cf2e06b812537a1ea437
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-10-12 14:37:03 +0200

    Fixed role checking

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,8 +4,18 @@ import { useState, useEffect } from "react";
 import { supabase } from '../lib/supabaseClient';
 import { Session } from "@supabase/supabase-js";
 
+type RoleData = {
+  roles: {
+    [key: string]: string;
+  };
+  userRoles: string[];
+  isAdmin: boolean;  
+};
+
+
 const Nav = () => {
   const [session, setSession] = useState<Session | null>(null)
+  const [roleData, setRoleData] = useState<RoleData | null>(null);
 
     useEffect(() => {
       supabase.auth.getSession().then(({ data: { session } }) => {
@@ -34,26 +44,40 @@ const Nav = () => {
       const { error } = await supabase.auth.signOut()
     }
    //console.log(session)
-   const [roleData, setRoleData] = useState(null);
 
    useEffect(() => {
     // Guard clause: return if session is null
     if (!session) return;
-
+  
     const discordUserId = session.user.user_metadata.sub;
     const guildId = '919622034546372679';
-
+  
     axios.get(`/api/userRoles?userId=${discordUserId}&guildId=${guildId}`)
-      .then(response => {
-        if (response.status !== 200) {
-          throw new Error('Network response was not ok');
-        }
-        setRoleData(response.data)
-        console.log(response.data); // will log the received roleData object
-      })
-      .catch(error => console.error('Error:', error));
-
-  }, [session]);
+  .then(response => {
+    if (response.status !== 200) {
+      throw new Error('Network response was not ok');
+    }
+    setRoleData(prevState => {
+      if (prevState) {
+        return {
+          roles: prevState.roles,
+          userRoles: prevState.userRoles,
+          isAdmin: response.data.isAdmin
+        };
+      } else {
+        // Assuming default values for roles and userRoles
+        return {
+          roles: {},
+          userRoles: [],
+          isAdmin: response.data.isAdmin
+        };
+      }
+    });
+    console.log("user is admin:", response.data.isAdmin); 
+  })
+  .catch(error => console.error('Error:', error));
+  
+  }, [session]);  
 
   return (
     <nav className="routes">
@@ -64,6 +88,11 @@ const Nav = () => {
         <Link href='/meeting-summaries' className="navitems">
           Upload Meeting Summaries
         </Link>
+        {roleData?.isAdmin && (
+          <Link href='/admin-panel' className="navitems">
+            Admin Panel
+          </Link>
+        )}
         <Link href='/contact' className="navitems">
           Contact
         </Link>
--- pages/api/userRoles.tsx
+++ pages/api/userRoles.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   const discordToken = process.env.DISCORD_BOT_TOKEN; // Your bot token
   const discordUserId = req.query.userId; // Get the userId from the query parameters
   const guildId = req.query.guildId; // Get the guildId from the query parameters
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       }
     });
 
-    const rolesResponse = await axios.get(`https://discord.com/api/guilds/${guildId}/roles`, {
+    /*const rolesResponse = await axios.get(`https://discord.com/api/guilds/${guildId}/roles`, {
       headers: {
         'Authorization': `Bot ${discordToken}`
       }
@@ -21,10 +21,15 @@ export default async function handler(req, res) {
     const roles = rolesResponse.data.reduce((obj, role) => ({
       ...obj,
       [role.id]: role.name,  // maps id to name
-    }), {});
+    }), {});*/
 
-    res.status(200).json({ userRoles: memberResponse.data.roles, roles }); // send the userRoles and roles back
-  } catch (error) {
+    const userRoles = memberResponse.data.roles;
+
+    // Check if user has the admin role
+    const isAdmin: any = userRoles.includes("972427271153516574");
+
+    res.status(200).json({ isAdmin }); // send the isAdmin status back
+  } catch (error: any) {
     console.error('Error:', error);
     res.status(error.response.status).json({ message: error.message });
   }
