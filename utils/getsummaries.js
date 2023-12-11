f483443fa5e80fb82bbc635f3f2535ae582cc9f5
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-12-11 15:09:59 +0200

    Added feature to remove meetings from dropdown when they are confirmed

--- utils/getsummaries.js
+++ utils/getsummaries.js
@@ -3,22 +3,37 @@ import { supabase } from "../lib/supabaseClient";
 export async function getSummaries(workgroup_id) {
   let summaries = []; // Initialize an array to hold the summaries
   let summary = {};
+  let lastConfirmedDate = null;
+  let lastUpdatedAt = null;
+  let lastConfirmedMeeting = null;
+
   async function getMeetingSummaries() {
     try {
-      // Fetch the last 6 summaries from the database
+      // Fetch all summaries from the database
       const { data: summaryData, error, status } = await supabase
         .from('meetingsummaries')
         .select('template, date, summary, user_id, meeting_id, confirmed, updated_at')
         .eq('workgroup_id', workgroup_id)
         .order('date', { ascending: false })  
-        .order('updated_at', { ascending: false }) 
-        .limit(6);
+        .order('updated_at', { ascending: false });
 
       if (error && status !== 406) throw error;
 
       if (summaryData) {
+        // Find the last confirmed meeting
+        lastConfirmedMeeting = summaryData.find(data => data.confirmed === true);
+        if (lastConfirmedMeeting) {
+          lastConfirmedDate = lastConfirmedMeeting.date;
+          lastUpdatedAt = lastConfirmedMeeting.updated_at;
+        }
+
         // Fetch the user details for each summary
         for (const data of summaryData) {
+          // Exclude meetings where confirmed == true and all meetings with an earlier date and updated_at date than the last meeting where confirmed == true
+          if (data.confirmed === true || new Date(data.date) < new Date(lastConfirmedDate) || new Date(data.updated_at) < new Date(lastUpdatedAt)) {
+            continue;
+          }
+
           const { data: userData, error: userError } = await supabase
             .from('users')
             .select('full_name')
@@ -29,7 +44,6 @@ export async function getSummaries(workgroup_id) {
           if (userData) {
             summary = data.summary
             // Add the summary and user details to the summaries array
-
             summaries.push({
               username: userData.full_name,
               meeting_id: data.meeting_id,
@@ -48,6 +62,30 @@ export async function getSummaries(workgroup_id) {
   }
 
   await getMeetingSummaries();
+
+  if (summaries.length === 0 && lastConfirmedMeeting) {
+    // Fetch the user details for the last confirmed meeting
+    const { data: userData, error: userError } = await supabase
+      .from('users')
+      .select('full_name')
+      .eq('user_id', lastConfirmedMeeting.user_id)
+      .single();
+
+    if (userError) throw userError;
+    if (userData) {
+      summary = lastConfirmedMeeting.summary
+      // Add the summary and user details to the last confirmed meeting
+      lastConfirmedMeeting = {
+        username: userData.full_name,
+        meeting_id: lastConfirmedMeeting.meeting_id,
+        confirmed: lastConfirmedMeeting.confirmed,
+        date: lastConfirmedMeeting.date,
+        updated_at: lastConfirmedMeeting.updated_at,
+        ...summary
+      };
+      summaries.push(lastConfirmedMeeting);
+    }
+  }
 
   return summaries; // Return the array of summaries
 }
