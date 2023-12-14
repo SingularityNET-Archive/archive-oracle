31f69b9097802b65e1a06d1ba9c1cb1facd3fda3
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-12-14 06:35:04 +0200

    Updated API to allow other origins

--- pages/api/getMeetingSummaries.js
+++ pages/api/getMeetingSummaries.js
@@ -4,6 +4,14 @@ export default async function handler(req, res) {
   const API_KEY = process.env.SERVER_API_KEY;
   const apiKeyHeader = req.headers['api_key'];
 
+  // Handle OPTIONS requests for CORS preflight
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+    return res.status(200).end();
+  }
+
   if (!apiKeyHeader || apiKeyHeader !== API_KEY) {
     return res.status(403).json({ error: 'Forbidden' });
   }
