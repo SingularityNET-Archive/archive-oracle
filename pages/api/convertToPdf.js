f57f55a509eddc746f537da36f385d54921b979d
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-12-24 05:32:13 +0200

    Remove puppeteer dependency and update PDF conversion logic

--- pages/api/convertToPdf.js
+++ pages/api/convertToPdf.js
@@ -1,31 +1,25 @@
-// pages/api/convertToPdf.js
-import { mdToPdf } from 'md-to-pdf';
-import puppeteer from 'puppeteer';
+import axios from 'axios';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const { markdown } = req.body;
 
-      // Start a new browser instance
-      const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'], // Necessary for certain deployment platforms
+      // Forward the request to md-to-pdf service
+      const pdfResponse = await axios.post('https://md-to-pdf.fly.dev', new URLSearchParams({ markdown }), {
+        responseType: 'arraybuffer', // To handle binary data
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
       });
 
-      const pdf = await mdToPdf({ content: markdown, browser }).catch(console.error);
-
-      // Close the browser instance after you're done with it
-      await browser.close();
-
-      if (pdf) {
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', 'attachment; filename=Meeting-Summary.pdf');
-        res.send(pdf.content);
-      } else {
-        res.status(500).json({ message: 'Error generating PDF' });
-      }
+      // Send the response back to the client
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename=Meeting-Summary.pdf');
+      res.send(pdfResponse.data);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Error generating PDF' });
     }
   } else {
     res.setHeader('Allow', ['POST']);
