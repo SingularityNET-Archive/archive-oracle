123ebc268f7e89e8d20c9053106965193ada452e
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-02-14 05:21:59 +0200

    Issues page is showing closed pull requests - [Date: 2024-02-13] Fixes #69

--- pages/api/issues.js
+++ pages/api/issues.js
@@ -6,14 +6,17 @@ export default async function handler(req, res) {
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
     try {
-        const issues = await octokit.paginate(octokit.rest.issues.listForRepo, {
+        const allIssuesAndPRs = await octokit.paginate(octokit.rest.issues.listForRepo, {
             owner: 'SingularityNET-Archive',
             repo: 'archive-oracle',
             state: 'all',
             per_page: 100, // Adjust per_page to your needs
         });
 
-        res.status(200).json(issues);
+        // Filter out pull requests from the issues
+        const issuesOnly = allIssuesAndPRs.filter(issue => !issue.pull_request);
+
+        res.status(200).json(issuesOnly);
     } catch (error) {
         console.error("Failed to fetch issues:", error);
         res.status(500).json({ error: error.message });
