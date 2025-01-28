8cd360e554587c81d5852e02854d7db64dbb637f
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2025-01-28 12:45:44 +0200

    refactor: Enhance sanitizeObject function to escape double quotes for valid JSON

--- netlify/functions/batchUpdateMeetingSummariesById.js
+++ netlify/functions/batchUpdateMeetingSummariesById.js
@@ -7,15 +7,17 @@ const MAX_CONCURRENT_REQUESTS = 10;
 
 function sanitizeObject(item) {
   if (typeof item === 'string') {
-    // Replace line breaks with spaces
-    // Only replace en dash (U+2013) or em dash (U+2014) with ASCII hyphen '-'
-    // Replace curly single quotes ’ and ‘ with straight ASCII apostrophe '
-    // Replace curly double quotes ” and “ with straight ASCII quote "
+    // 1. Remove line breaks
+    // 2. Replace fancy dashes with '-'
+    // 3. Replace curly single quotes ’ and ‘ with straight apostrophe '
+    // 4. Replace curly double quotes ” and “ with straight ASCII quote "
+    // 5. Escape any new " characters to \" for valid JSON
     return item
       .replace(/\r?\n/g, ' ')
       .replace(/[\u2013\u2014]/g, '-')
       .replace(/[\u2018\u2019]/g, "'")
       .replace(/[\u201C\u201D]/g, '"')
+      .replace(/"/g, '\\"')
       .trim();
   } 
   
