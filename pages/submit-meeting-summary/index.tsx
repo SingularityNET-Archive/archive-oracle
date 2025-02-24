e8f3fd20dd775afb176fa87bf4861dab17c097f7
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2025-02-24 14:33:50 +0200

    refactor: Remove unnecessary console log in SubmitMeetingSummary component

--- pages/submit-meeting-summary/index.tsx
+++ pages/submit-meeting-summary/index.tsx
@@ -520,8 +520,7 @@ const SubmitMeetingSummary: NextPage = () => {
     const meetingDate = m.meetingInfo?.date || m.date.split("T")[0];
     return meetingDate === newSummaryDate && m.username === myVariable.currentUser;
   });
-
- console.log(myVariable, meetings, newSummaryDate)
+  
   return (
     <div className={styles.container}>
       {/* ---------- MODAL ---------- */}
